0e328251c46589824f870475f3d74350dc45d175
diegoapb <parra.diego@correounivalle.edu.co>
2021-11-16 19:57:55 -0500

    Initial commit

---
+++ test/demo-cognito-api.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as DemoCognitoApi from '../lib/demo-cognito-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-cognito-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoCognitoApi.DemoCognitoApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
